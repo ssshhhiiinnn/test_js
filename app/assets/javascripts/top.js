750cca19d39a01fc6cf9e6e891ce4f848af22de1
Shinnosuke Azuma <shinnosukeazuma@Shinnosuke-no-MacBook-Pro.local>
2017-10-18 14:32:00 +0900

    関数をもっと見やすい形に変更

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,14 +1,39 @@
-function printHelloWithButton() {
+// function printHelloWithButton() {
+//   var btn = document.querySelector("input");
+//
+//   function printHello() {
+//     console.log("Hello world");
+//   }
+//   // 関数内で定義された関数は、関数の中でしか呼び出せないという性質があるだけで、
+//   // 通常の関数同様に呼び出せる
+//
+//   btn.addEventListener("click", printHello);
+// }
+// // 一連の処理をまとめた関数を作る
+//
+// window.addEventListener("load", printHelloWithButton);
+
+
+
+
+
+// window.addEventListener("load", function() {
+//   var btn = document.querySelector("input");
+//
+//   function printHello() {
+//     console.log("Hello world");
+//   }
+//
+//   btn.addEventListener("click", printHello);
+// });
+
+
+
+
+window.addEventListener("load", function() {
   var btn = document.querySelector("input");
 
-  function printHello() {
+  btn.addEventListener("click", function() {
     console.log("Hello world");
-  }
-  // 関数内で定義された関数は、関数の中でしか呼び出せないという性質があるだけで、
-  // 通常の関数同様に呼び出せる
-
-  btn.addEventListener("click", printHello);
-}
-// 一連の処理をまとめた関数を作る
-
-window.addEventListener("load", printHelloWithButton);
+  });
+});
