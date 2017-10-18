ca23456ffb243db8bfb18758beec3e4f06774bc6
Shinnosuke Azuma <shinnosukeazuma@Shinnosuke-no-MacBook-Pro.local>
2017-10-18 14:10:19 +0900

    ボタンをクリックしたら文字が表示される機能

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+function printHelloWithButton() {
+  var btn = document.querySelector("input");
+
+  function printHello() {
+    console.log("Hello world");
+  }
+  // 関数内で定義された関数は、関数の中でしか呼び出せないという性質があるだけで、
+  // 通常の関数同様に呼び出せる
+
+  btn.addEventListener("click", printHello);
+}
+// 一連の処理をまとめた関数を作る
+
+window.addEventListener("load", printHelloWithButton);
